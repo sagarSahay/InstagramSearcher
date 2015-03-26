01daf40b0fbb599692ee703fa61f39d0adc0a2fa
Sagar <sagar.sahay@gmail.com>
2015-03-26 17:14:31 +0000

    starting to add promises

--- scripts/app.js
+++ scripts/app.js
@@ -4,23 +4,32 @@
 angular.module('InstagramApp',[])
     .controller('InstagramController',function($scope,$http,$q){
               $scope.keyword='';
+        $scope.count=0;
 
-        $scope.submit=function(){
+        function callInstagram(){
+
             var clientId='2a4fac4d6fbb48b890085ad5f873404a';
             var url='https://api.instagram.com/v1/tags/'+$scope.keyword+'/media/recent';
             var request={
-                jsonp:"JSON_CALLBACK",
+                callback:"JSON_CALLBACK",
                 client_id:clientId
             };
+            var defer =$q.defer();
+
             $http({
                 method: 'JSONP',
                 url: url,
                 params:request
             }).success(function(result) {
-                    $scope.data = result;
-                }).error(function(data) {
-                    alert('error');
-                })
+                $scope.data = result;
+                $scope.count=result.length;
+            }).error(function(data) {
+                alert('error');
+            })
+        }
+
+        $scope.submit=function(){
+
 
         };
     });
