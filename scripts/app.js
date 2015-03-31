72fc41b10460cc43e39b4b106667c9528555de9f
Sagar <sag.sahay@gmail.com>
2015-03-31 23:20:41 +0100

    Separating the web call into service

--- scripts/app.js
+++ scripts/app.js
@@ -2,25 +2,45 @@
  * Created by Sagar on 25/03/2015.
  */
 angular.module('InstagramApp',[])
-    .controller('InstagramController',function($scope,$http,$q){
-              $scope.keyword='';
+    .controller('InstagramController',function(InstagramService,$sce){
+        var vm=this;
+        vm.keyword='';
+        vm.count=0;
+        vm.data=null;
 
-        $scope.submit=function(){
-            var clientId='2a4fac4d6fbb48b890085ad5f873404a';
-            var url='https://api.instagram.com/v1/tags/'+$scope.keyword+'/media/recent';
-            var request={
-                jsonp:"JSON_CALLBACK",
-                client_id:clientId
-            };
-            $http({
-                method: 'JSONP',
-                url: url,
-                params:request
-            }).success(function(result) {
-                    $scope.data = result;
-                }).error(function(data) {
-                    alert('error');
-                })
+        vm.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        };
+        vm.submit=function(){
+            InstagramService.get(vm.keyword).then(function (result) {
+                vm.data=result.data;
+            }).catch(function (error) {
+                console.log(error);
+            })
 
         };
+    }).service('InstagramService',function($q,$http){
+        var result=null;
+        return {
+            'get':function(keyword){
+                var deferred =$q.defer();
+                var clientId='3e147fa6d878423d904ab741726c67a1';
+                var url='https://api.instagram.com/v1/tags/'+keyword+'/media/recent';
+                var request={
+                    callback:"JSON_CALLBACK",
+                    client_id:clientId
+                };
+                $http({
+                    method: 'JSONP',
+                    url: url,
+                    params:request
+                }).success(function(response) {
+                    result=response;
+                    deferred.resolve(result);
+                }).error(function(error) {
+                    deferred.reject(error) ;
+                })
+                return deferred.promise;
+            }
+        }
     });
