559e42d3608e2817e207a320099731eaa346a30e
Sagar <sag.sahay@gmail.com>
2015-04-04 18:37:05 +0100

    Adding click evetns

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,57 @@
 /**
  * Created by Sagar on 25/03/2015.
  */
-angular.module('InstagramApp',[])
-    .controller('InstagramController',function(InstagramService,$sce){
-        var vm=this;
-        vm.keyword='';
-        vm.count=0;
-        vm.data=null;
+angular.module('InstagramApp', [])
+    .controller('InstagramController', function (InstagramService, $sce, $scope,$window) {
+        var vm = this;
+        vm.keyword = '';
+        vm.display = false;
+        vm.count = 0;
+        vm.data = null;
 
-        vm.trustSrc = function(src) {
+        vm.trustSrc = function (src) {
             return $sce.trustAsResourceUrl(src);
         };
-        vm.submit=function(){
+        vm.submit = function () {
             InstagramService.get(vm.keyword).then(function (result) {
-                vm.data=result.data;
-                vm.count=result.data.length;
+                vm.data = result.data.data;
+                vm.count = result.data.data.length;
+                vm.display = true;
             }).catch(function (error) {
                 console.log(error);
             })
 
         };
-    }).service('InstagramService',function($q,$http){
-        var result=null;
+        $scope.$watch('vm.keyword', function (str) {
+            if (str === '') {
+                vm.display = false;
+            }
+        });
+        $scope.openLinkWindow = function (link) {
+            $window.open(link);
+        };
+    }).service('InstagramService', function ($http) {
+        var result = null;
         return {
-            'get':function(keyword){
-                var deferred =$q.defer();
-                var clientId='3e147fa6d878423d904ab741726c67a1';
-                var url='https://api.instagram.com/v1/tags/'+keyword+'/media/recent';
-                var request={
-                    callback:"JSON_CALLBACK",
-                    client_id:clientId
-                };
-            var defer =$q.defer();
+            'get': function (keyword) {
 
-                $http({
+                var clientId = '3e147fa6d878423d904ab741726c67a1';
+                var url = 'https://api.instagram.com/v1/tags/' + keyword + '/media/recent';
+                var request = {
+                    callback: "JSON_CALLBACK",
+                    client_id: clientId
+                };
+
+
+                return $http({
                     method: 'JSONP',
                     url: url,
-                    params:request
-                }).success(function(response) {
-                    result=response;
-                    deferred.resolve(result);
-                }).error(function(error) {
-                    deferred.reject(error) ;
-            });
-                return deferred.promise;
+                    params: request
+                }).success(function (response) {
+                    result = response;
+                }).error(function (error) {
+                    result = error;
+                });
             }
         }
 
